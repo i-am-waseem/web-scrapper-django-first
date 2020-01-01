7f744d72620dfa3ccf94f8ed647595cb4cd23cc1
i-am-waseem <49545568+i-am-waseem@users.noreply.github.com>
2020-01-02 00:53:37 +0530

    Update settings.py

--- craiglist/craiglist/settings.py
+++ craiglist/craiglist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v#=b+i3_a_h&z3)@y9tam4#18#(y_%_r2uua%&&jy=3*@n0*dz'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
